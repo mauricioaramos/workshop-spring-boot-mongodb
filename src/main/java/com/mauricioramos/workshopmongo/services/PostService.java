27b77427655320f3030ccff23e8c24e632b69a5c
Mauricio Ramos <mauramosaraujo@gmail.com>
2021-04-18 08:54:04 -0300

    Obter post por id

---
+++ src/main/java/com/mauricioramos/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.mauricioramos.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mauricioramos.workshopmongo.domain.Post;
+import com.mauricioramos.workshopmongo.domain.User;
+import com.mauricioramos.workshopmongo.repository.PostRepository;
+import com.mauricioramos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById( String id ) {
+		Optional<Post> postOptional = repo.findById(id);
+		return postOptional.orElseThrow(() -> new ObjectNotFoundException(id) );
+		
+	}
+	
+	
+	
+}
